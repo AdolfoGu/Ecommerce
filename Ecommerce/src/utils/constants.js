a2051cddd2db044ce72c8e42ffd46fd6be65cad3
Adolfo <ja.gutierrez.gaytan@ugto.mx>
2022-02-07 10:50:43 -0600

    Primer Commit
    primer commit

---
+++ Ecommerce/src/utils/constants.js
@@ -0,0 +1,7 @@
+export const API_URL = "http://192.168.1.71:1337";
+// export const API_URL = "https://react-native-ecommerce-dev.herokuapp.com";
+export const TOKEN = "token";
+export const SEARCH_HISTORY = "search-history";
+export const CART = "cart";
+export const STRAPI_PUBLISHABLE_KEY =
+  "pk_test_51KNLSfJbwAEarsJ5wqgD8l8bAHP9oJiXxRNzFklH4TkekLNWB3nOqdgczXfXkCuijFOiRCwHoqmWOh3vVXfzxY2Z00iiWmshvT";
